288d0283b475472b4f5d3aee733bb048b53f6f17
NathaNaeru <nathanael.leander@ui.ac.id>
2026-05-21 12:27:07 +0700

    Refactor ApiGatewayApplication and application.yml to resolve merge conflicts and clean up configuration

--- src/main/java/id/ac/ui/cs/advprog/yomu/gateway/ApiGatewayApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/yomu/gateway/ApiGatewayApplication.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package id.ac.ui.cs.advprog.yomu.gateway;
 
 import org.springframework.boot.SpringApplication;
@@ -32,38 +31,3 @@ public class ApiGatewayApplication {
 
 }
 
-=======
-package id.ac.ui.cs.advprog.yomu.gateway;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-@SpringBootApplication(excludeName = {
-    "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration",
-    "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration",
-    "org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration",
-    "org.springframework.cloud.autoconfigure.LifecycleMvcEndpointAutoConfiguration",
-    "org.springframework.cloud.autoconfigure.RefreshAutoConfiguration",
-    "org.springframework.cloud.client.discovery.simple.SimpleDiscoveryClientAutoConfiguration",
-    "org.springframework.cloud.client.discovery.simple.reactive.SimpleReactiveDiscoveryClientAutoConfiguration",
-    "org.springframework.cloud.client.loadbalancer.LoadBalancerAutoConfiguration",
-    "org.springframework.cloud.client.CommonsClientAutoConfiguration",
-    "org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClientAutoConfiguration",
-    "org.springframework.cloud.client.serviceregistry.ServiceRegistryAutoConfiguration",
-    "org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration",
-    "org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration",
-    "id.ac.ui.cs.advprog.yomu.shared.config.SharedSecurityConfig"
-})
-@ComponentScan(basePackages = {
-    "id.ac.ui.cs.advprog.yomu.gateway"
-})
-public class ApiGatewayApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
-    }
-
-}
-
->>>>>>> bc0a42f3cb4f65a30b5f6bc8a72742027e35172f
