933c04bd8b23f43302a292e1210f5d5d3a216bc7
Christna Yosua Rotinsulu <yosuamic123@gmail.com>
2026-05-22 12:25:11 +0700

    [Config] Mengubah konfigurasi SonarQube Project dan Organization Key

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,8 +61,8 @@ tasks.jacocoTestReport {
 
 sonar {
     properties {
-        property("sonar.projectKey", "yomu-app-api-gateway")
-        // property("sonar.organization", "your-organization-key") // Uncomment if using SonarCloud
+        property("sonar.projectKey", "advprog-2026-A15-project_yomu-api-gateway")
+        property("sonar.organization", "advprog-2026-a15-project")
         property("sonar.host.url", System.getenv("SONAR_HOST_URL") ?: "https://sonarcloud.io")
         property("sonar.token", System.getenv("SONAR_TOKEN") ?: "")
         property("sonar.coverage.jacoco.xmlReportPaths", "${layout.buildDirectory.get()}/reports/jacoco/test/jacocoTestReport.xml")
